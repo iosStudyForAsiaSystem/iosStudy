2f6d4e3cbaf09de268e3a2a0c550c347e0f820a1
snowman <from21cjp@yahoo.co.jp>
2017-02-09 18:23:48 +0900

    add playground project add sample code (objective-c, swift3)

--- workspace/gitServerObjCSample/gitServerObjCSample/FirstViewController.m
+++ workspace/gitServerObjCSample/gitServerObjCSample/FirstViewController.m
@@ -25,5 +25,54 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) debug_print {
+    
+    NSString* msg = @"こんにちは";
+    int boy = 12;
+    int girl = 10;
+    id array = @[@"a", @"b", @"c"];
+    
+
+    
+    // 出力する
+    NSLog(@"%@", msg);
+    NSLog(@"%d%d", boy, girl); // 複数の値をカンマで区切る
+    NSLog(@"男子 %d ,女子 %d", boy, girl);
+    NSLog(@"合計 %d ", boy+ girl);
+    NSLog(@"%@",array); // 配列
+}
+
+- (void) debug_var {
+    int  kokugo = 56;
+    int sansu = 67;
+    int  goukei;
+    kokugo = 56;
+    sansu = 67;
+    goukei = kokugo + sansu;
+    NSLog(@"%d ", goukei);
+}
+
+- (void ) separator_test {
+    NSString* msg1 = @"こんにちは";
+    NSString* msg2 = @"ありがとう";
+    NSString* msg3 = @"さようなら";
+    NSLog(@"%@／%@／%@/", msg1, msg2, msg3);
+}
+
+- (void) kuji {
+    // 1〜10の乱数を作る
+    int num = arc4random_uniform(10)+1;
+    // numの値で処理を分岐する
+    if (num>=7) {
+        // numが7以上の時
+        NSString* msg = @"あたり";
+        NSLog(@"%d,%@",num, msg);
+    } else {
+        // numが7未満の時
+        NSString* msg = @"はずれ";
+         NSLog(@"%d,%@",num, msg);
+    }
+}
+
 
 @end
---
+++ workspace/samplePlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var goukei = 0
+
+for num in 1...10 {
+    goukei = goukei + num
+
+    
+}
+
+for kakudo in 0..<360 {
+    let radian = Double(kakudo) * M_PI/180
+    let y1 = sin (radian)
+    
+    let y2 = sin (radian*3)
+    let y3 = cos (radian*3)
+}
+
