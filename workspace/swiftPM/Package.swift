6bd93f3ba66e511d8c3635373ef5a16273f78c71
snowman <from21cjp@yahoo.co.jp>
2017-02-20 14:46:22 +0900

    commit - current project

--- workspace/swiftPM/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "swiftPM",
-    dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
-        ]
-)
--- workspace/swiftPM/Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import swiftPMTests
-
-XCTMain([
-     testCase(swiftPMTests.allTests),
-])
