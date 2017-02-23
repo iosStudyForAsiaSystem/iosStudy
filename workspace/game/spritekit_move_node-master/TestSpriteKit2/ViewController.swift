5281e102f72dbec585691347764ff70bfcd525c9
snowman <from21cjp@yahoo.co.jp>
2017-02-23 17:02:56 +0900

    fix TestSpriteKit - sks file

--- workspace/game/spritekit_move_node-master/TestSpriteKit2/ViewController.swift
+++ workspace/game/spritekit_move_node-master/TestSpriteKit2/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
         let skView = self.view as! SKView
         
         //SKViewと同じサイズのSKSceneインスタンスを生成する。
-        let scene = TestScene(size:skView.frame.size)
+        //let scene = TestScene(size:skView.frame.size)
+        let scene = TestScene(fileNamed: "TestScene")
+        
+        
+        scene?.scaleMode = .aspectFill
         
         //現在シーンを設定する。
         skView.presentScene(scene)
